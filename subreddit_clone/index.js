777f1adc9794d0c21589c694d5eb1b0e5ad8fc7f
aaryan-sood <soodaaryan7@gmail.com>
2024-06-12 21:53:07 +0530

    commited the dmmy subreddit demo

--- subreddit_clone/index.js
+++ subreddit_clone/index.js
@@ -1,9 +1,24 @@
 const express=require('express')
 const path=require('path')
-let port=8080
-app.set('views',path.join(__dirname,'/views'))
 app=express()
+let port=8080
+app.set('view engine','ejs')
+
+const redditData=require('./data.json')
+
+app.set('views',path.join(__dirname,'/views'))
 app.listen(port,() => {
     console.log(`listening on port : ${port}`)
 })
-app.set('view engine','ejs')
+app.get('/r/:subreddit',(req,res) => {
+    let {subreddit}=req.params;
+    let data=redditData[subreddit]
+    if(data){
+        res.render('subreddit.ejs',{data})
+        console.log(data)
+    }
+    else{
+        res.render('notfound.ejs',{subreddit})
+    }
+    
+})
