e2c546dcc18aaa3e6ea397b65189fbb83331a503
aaryan-sood <soodaaryan7@gmail.com>
2024-06-13 18:09:32 +0530

    learnt to add bootstrap in express and the usage of partials in ejs

--- bootstrap_demo/index.js
+++ bootstrap_demo/index.js
@@ -5,13 +5,19 @@ let port=8080
 const redditData=require('./data.json')
 
 app.use(express.static(path.join(__dirname,'public')))
+
 app.set('view engine','ejs')
-
-
 app.set('views',path.join(__dirname,'/views'))
 
 app.listen(port,() => {
     console.log(`listening on port : ${port}`)
+})
+app.get('/rand',(req,res) => {
+    let random=Math.floor(Math.random() * 10) + 1
+    res.render('random.ejs',{random})
+})
+app.get('/',(req,res) => {
+    res.render('home.ejs')
 })
 app.get('/r/:subreddit',(req,res) => {
     let {subreddit}=req.params;
